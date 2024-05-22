cd70c0a7fdf29d0a91557a12c2ce82ce7d53087d
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-05-22 14:35:14 -0400

    Remove nav bar from portfolio page

--- springer-sandbox/src/pages/portfolio.js
+++ springer-sandbox/src/pages/portfolio.js
@@ -25,7 +25,6 @@ const Portfolio = () => {
   return (
     <>
         <Sidebar isOpen={isOpen} toggle={toggle} />
-        <Navbar toggle={toggle} />
         <PortfolioSection />
        <Footer />
     </>
