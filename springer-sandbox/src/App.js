e1a406a60f2f7c458010ad91ae68cd20182685a6
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-05-22 13:40:04 -0400

    Router and page update

--- springer-sandbox/src/App.js
+++ springer-sandbox/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages';
 import Portfolio from './pages';
@@ -8,7 +8,10 @@ import Portfolio from './pages';
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </Router>
   );
 }
