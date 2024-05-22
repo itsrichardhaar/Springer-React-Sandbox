9d0bd8e88087d48a5865b204a881c17b15a38b7d
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-05-22 13:44:32 -0400

    Update app.js routes

--- springer-sandbox/src/App.js
+++ springer-sandbox/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages';
-import Portfolio from './pages';
+import Portfolio from './pages/portfolio';
 
 function App() {
   return (
