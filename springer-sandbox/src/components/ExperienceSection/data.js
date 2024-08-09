6c4124735d79ad24431c5533174f113e18f8bc47
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-08-09 15:58:47 -0400

    Update data.js
    Copy changes for experience desription

--- springer-sandbox/src/components/ExperienceSection/data.js
+++ springer-sandbox/src/components/ExperienceSection/data.js
@@ -6,7 +6,7 @@ export const ExperienceObjOne = {
     topLine: 'Crafting Digital Solutions',
     headline: 'Work Experience',
     description:
-      'My experience includes collaborating closely with design teams to translate mockups into functional websites, ensuring pixel-perfect accuracy and optimal user experience across various devices and browsers.',
+      'My experience includes collaborating closely with design teams to translate mockups into functional websites and applications, ensuring pixel-perfect accuracy and optimal user experience across various devices and browsers.',
     buttonLabel: 'View Full Resume',
     imgStart: true,
     img: require('../../images/svg-2.svg'),
